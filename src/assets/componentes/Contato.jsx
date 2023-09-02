4ecf45691745bf2cb7ed710b6f5a7baa2e717e93
Deivid Barreto <deividbarreto57@gmail.com>
2023-09-01 23:19:37 -0300

    criando a página de serviços

--- src/assets/componentes/Contato.jsx
+++ src/assets/componentes/Contato.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Cabecalho from '../componentes/Cabecalho'
+import {BiLogoFacebook} from 'react-icons/bi'
+import {FaTwitter} from 'react-icons/fa'
+import {FaYoutube} from 'react-icons/fa'
 
 export default function Contato() {
 
@@ -13,6 +16,46 @@ export default function Contato() {
                     >Contato</h2>
                 </div>
             </div>
+
+            <h2>Sou um título. Clique aqui para editar e adicionar o seu próprio texto.</h2>
+
+            <section>
+                <form>
+                    <div>
+                        <input type="text" name="nome" placeholder='Nome' />
+                        <input type="tel" name="telefone" placeholder='Telefone' />
+                    </div>
+                    <input type="email" name="email" placeholder='Email' />
+                    <textarea name="mensagem" placeholder='Digite uma mensagem aqui...' ></textarea>
+                    <input type="submit" value="Enviar" />
+                </form>
+
+                <div>
+                    <div>
+                        <h3>ENTRE EM CONTATO</h3>
+                        <div>
+                            <span>(11) 3456-7890</span>
+                            <a href="#">info@meusite.com</a>
+                            <span>Av. Bernardino de Campos 98</span>
+                            <span>São Paulo, SP 12345-678</span>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <a href="#"><BiLogoFacebook /></a>
+                        <a href="#"><FaTwitter /></a>
+                        <a href="#"><FaYoutube /></a>
+                    </div>
+
+                    <div>
+                        <h2>PEÇA UM ORÇAMENTO GRÁTIS</h2>
+                        <div>
+                            <span>Precisa de um carpinteiro em São Paulo?</span>
+                            <span>Ligue agora: (11) 3456-7890</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- src/assets/componentes/Servicos.jsx
+++ src/assets/componentes/Servicos.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Cabecalho from '../componentes/Cabecalho'
+import imgFeitoAMao from '../imgs/feitoMao.webp'
+import imgReformas from '../imgs/reformas.webp'
+import imgManutencao from '../imgs/manutencao.webp'
 
 export default function Servicos() {
 
@@ -13,6 +16,44 @@ export default function Servicos() {
                     >Seviços</h2>
                 </div>
             </div>
+
+            <section className='flex flex-col md:items-center'>
+                <h2 className='text-cinzaEscuro font-playfair text-20 text-center px-5 my-10 md:text-27 md:pr-20 md:mt-20'>
+                    Sou um título. Clique aqui para editar e adicionar o seu próprio texto.
+                </h2>
+
+                <section className='flex flex-col gap-10 items-center md:flex-row md:items-center md:justify-center md:h-screen md:gap-10'>
+                    <div className='w-larguraCardsServico'>
+                        <h3 className='text-30 text-amareloPrincipal font-grotesk font-semibold text-center'>
+                            MÓVEIS FEITOS<br /> À MÃO
+                        </h3>
+                        <img src={imgFeitoAMao} alt="Móveis" className='h-alturaImgsCardServico' />
+                        <p className='py-4 w-4/5 font-playfair text-15 text-textoQuemSomos'>
+                            Sou um parágrafo. Clique aqui para editar e adicionar o seu próprio texto. É fácil! Basta clicar em "Editar Texto" ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes.
+                        </p>
+                    </div>
+
+                    <div className='w-larguraCardsServico'>
+                        <h3 className='text-30 text-amareloPrincipal font-grotesk font-semibold text-center'>
+                            REFORMA<br /> PERSONALIZADA
+                        </h3>
+                        <img src={imgReformas} alt="Reforma" className='h-alturaImgsCardServico' />
+                        <p className='py-4 w-4/5 font-playfair text-15 text-textoQuemSomos'>
+                            Sou um parágrafo. Clique aqui para editar e adicionar o seu próprio texto. É fácil! Basta clicar em "Editar Texto" ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes.
+                        </p>
+                    </div>
+
+                    <div className='w-larguraCardsServico'>
+                        <h3 className='text-30 text-amareloPrincipal font-grotesk font-semibold text-center'>
+                            MANUTANÇAO DE<br /> IMÓVEIS
+                        </h3>
+                        <img src={imgManutencao} alt="Manutenção" className='h-alturaImgsCardServico' />
+                        <p className='py-4 w-4/5 font-playfair text-15 text-textoQuemSomos'>
+                            Sou um parágrafo. Clique aqui para editar e adicionar o seu próprio texto. É fácil! Basta clicar em "Editar Texto" ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes.
+                        </p>
+                    </div>
+                </section>
+            </section>
         </>
     )
 }
